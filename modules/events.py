ab6cfc1ddca4e0328e95d89f91ce438e030679da
Matthias Urlichs <smurf@smurf.noris.de>
2007-05-18 15:43:35 +0200

    Add "wait for ..." timeout handler to the events module

--- modules/events.py
+++ modules/events.py
@@ -12,6 +12,7 @@ trigger FOO...
 from homevent.parser import SimpleStatement, main_words
 from homevent.event import Event
 from homevent.run import process_event
+from twisted.internet import reactor,defer
 
 class TriggerHandler(SimpleStatement):
 	name=("trigger",)
@@ -24,7 +25,48 @@ trigger FOO...
 		w = w[len(self.name):]
 		if not w:
 			raise SyntaxError("Events need at least one parameter")
-		process_event(Event(self.ctx,*w))
+		return process_event(Event(self.ctx,*w))
+
+class WaitHandler(SimpleStatement):
+	name=("wait","for")
+	doc="delay for N seconds"
+	long_doc="""\
+wait for FOO...
+	- delay processsing for FOO seconds
+	  append "s/m/h/d/w" for seconds/minutes/hours/days/weeks
+	  # you can do basic +/- calculations (2m - 10s); you do need the spaces
+"""
+	def input(self,w):
+		w = w[len(self.name):]
+		print repr(w)
+		if not w:
+			raise SyntaxError("Timers need a value")
+		if len(w) > 2:
+			raise SyntaxError("Timers need ONE value",w)
+		m = 1
+		if len(w) == 1:
+			pass
+		elif w[1] in ("s","sec","second","seconds"):
+			pass
+		elif w[1] in ("m","min","minute","minutes"):
+			m = 60
+		elif w[1] in ("h","hr","hour","hours"):
+			m = 60*60
+		elif w[1] in ("d","dy","day","days"):
+			m = 60*60*24
+		elif w[1] in ("w","wk","week","weeks"):
+			m = 60*60*24*7
+		else:
+			raise SyntaxError("unknown unit",w[1])
+		w = m * w[0]
+		r = defer.Deferred()
+		print "S stop"
+		def cb():
+			print "S go"
+			r.callback(None)
+		reactor.callLater(w,cb)
+		#reactor.callLater(w,r.callback,None)
+		return r
 
 from homevent.module import Module
 from homevent.logging import log
@@ -38,8 +80,10 @@ class EventsModule(Module):
 
 	def load(self):
 		main_words.register_statement(TriggerHandler)
+		main_words.register_statement(WaitHandler)
 	
 	def unload(self):
 		main_words.unregister_statement(TriggerHandler)
+		main_words.unregister_statement(WaitHandler)
 
 init = EventsModule
