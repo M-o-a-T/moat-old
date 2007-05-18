f8c74ef92599766f71d3536c6e0df0ea7cefafd3
Matthias Urlichs <smurf@smurf.noris.de>
2007-05-18 17:12:31 +0200

    changed events.waitfor module to allow +/- times (10min-5sec) added a test case

--- modules/events.py
+++ modules/events.py
@@ -12,6 +12,7 @@ trigger FOO...
 from homevent.parser import SimpleStatement, main_words
 from homevent.event import Event
 from homevent.run import process_event
+from homevent.logging import log,TRACE
 from twisted.internet import reactor,defer
 
 class TriggerHandler(SimpleStatement):
@@ -27,6 +28,8 @@ trigger FOO...
 			raise SyntaxError("Events need at least one parameter")
 		return process_event(Event(self.ctx,*w))
 
+timer_nr = 0
+
 class WaitHandler(SimpleStatement):
 	name=("wait","for")
 	doc="delay for N seconds"
@@ -36,37 +39,64 @@ wait for FOO...
 	  append "s/m/h/d/w" for seconds/minutes/hours/days/weeks
 	  # you can do basic +/- calculations (2m - 10s); you do need the spaces
 """
+	def __init__(self,*a,**k):
+		super(WaitHandler,self).__init__(*a,**k)
+		global timer_nr
+		timer_nr += 1
+		self.nr = timer_nr
+
 	def input(self,w):
 		w = w[len(self.name):]
-		print repr(w)
+		s = 0
 		if not w:
 			raise SyntaxError("Timers need a value")
-		if len(w) > 2:
-			raise SyntaxError("Timers need ONE value",w)
 		m = 1
-		if len(w) == 1:
-			pass
-		elif w[1] in ("s","sec","second","seconds"):
-			pass
-		elif w[1] in ("m","min","minute","minutes"):
-			m = 60
-		elif w[1] in ("h","hr","hour","hours"):
-			m = 60*60
-		elif w[1] in ("d","dy","day","days"):
-			m = 60*60*24
-		elif w[1] in ("w","wk","week","weeks"):
-			m = 60*60*24*7
-		else:
-			raise SyntaxError("unknown unit",w[1])
-		w = m * w[0]
-		if w < 0:
+		while w:
+			if len(w) == 1:
+				pass
+			elif w[1] in ("s","sec","second","seconds"):
+				w.pop(1)
+			elif w[1] in ("m","min","minute","minutes"):
+				m = 60
+				w.pop(1)
+			elif w[1] in ("h","hr","hour","hours"):
+				m = 60*60
+				w.pop(1)
+			elif w[1] in ("d","dy","day","days"):
+				m = 60*60*24
+				w.pop(1)
+			elif w[1] in ("w","wk","week","weeks"):
+				m = 60*60*24*7
+				w.pop(1)
+			elif w[1] in ("+","-"):
+				pass
+			else:
+				raise SyntaxError("unknown unit",w[1])
+			s += m * w[0]
+			w.pop(0)
+			if w:
+				if w[0] == "+":
+					w.pop(0)
+					m = 1
+				elif w[0] == "-":
+					w.pop(0)
+					m = -1
+				else:
+					m = 1 # "1min 59sec"
+					
+		if s < 0:
+			log(TRACE,"No time out:",s)
 			return # no waiting
+		log(TRACE,"Timer",self.nr,"::",s)
 		r = defer.Deferred()
-		reactor.callLater(w,r.callback,None)
+		def doit():
+			log(TRACE,"Timeout",self.nr)
+			r.callback(None)
+		reactor.callLater(s,doit)
 		return r
 
+
 from homevent.module import Module
-from homevent.logging import log
 
 class EventsModule(Module):
 	"""\
