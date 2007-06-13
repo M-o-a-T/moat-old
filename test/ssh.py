d950fc84ae1a42c398ee5527cf18b24de3eeb2df
Matthias Urlichs <smurf@smurf.noris.de>
2007-06-13 21:47:07 +0200

    New: SSH access to the whole thing

---
+++ test/ssh.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import homevent as h
+from homevent.reactor import ShutdownHandler
+from homevent.module import load_module,Load
+from homevent.statement import global_words
+from test import run
+
+global_words.register_statement(Load)
+
+
+input = """\
+load ssh
+block:
+	if directory exists "keys":
+		ssh directory "keys"
+	else:
+		ssh directory "../keys"
+
+# TODO: run an external test ‽
+shutdown
+"""
+
+h.main_words.register_statement(ShutdownHandler)
+load_module("block")
+load_module("path")
+load_module("ifelse")
+
+run("ssh",input)
+
