8725c39e6e3c9d0cf74a0c48de86fbf272870447
Matthias Urlichs <smurf@smurf.noris.de>
2007-06-13 20:53:15 +0200

    delete old commented stuff

--- modules/ssh.py
+++ modules/ssh.py
@@ -31,56 +31,6 @@ class SSHDemoProtocol(recvline.HistoricRecvLine):
 			self.transport = self.terminal
 		self.terminal.write("This is the HomEvenT command line.")
 		self.terminal.nextLine()
-#		self.do_help()
-#		self.showPrompt()
-#	def showPrompt(self):
-#		self.terminal.write(">> ")
-#	def getCommandFunc(self, cmd):
-#		return getattr(self, 'do_' + cmd, None)
-#		line = line.strip()
-#		if line:
-#			cmdAndArgs = line.split()
-#			cmd = cmdAndArgs[0]
-#			args = cmdAndArgs[1:]
-#			func = self.getCommandFunc(cmd)
-#			if func:
-#				try:
-#					func(*args)
-#				except Exception, e:
-#					self.terminal.write("Error: %s" % e)
-#					self.terminal.nextLine()
-#			else:
-#				self.terminal.write("No such command.")
-#				self.terminal.nextLine()
-#		self.showPrompt()
-#	def do_help(self, cmd=''):
-#		"Get help on a command. Usage: help command"
-#		if cmd:
-#			func = self.getCommandFunc(cmd)
-#			if func:
-#				self.terminal.write(func.__doc__)
-#				self.terminal.nextLine()
-#				return
-#		publicMethods = filter(lambda funcname: funcname.startswith('do_'), dir(self))
-#		commands = [cmd.replace('do_', '', 1) for cmd in publicMethods]
-#		self.terminal.write("Commands: " + " ".join(commands))
-#		self.terminal.nextLine()
-#	def do_echo(self, *args):
-#		"Echo a string. Usage: echo my line of text"
-#		self.terminal.write(" ".join(args))
-#		self.terminal.nextLine()
-#	def do_whoami(self):
-#		"Prints your user name. Usage: whoami"
-#		self.terminal.write(self.user.username)
-#		self.terminal.nextLine()
-#	def do_quit(self):
-#		"Ends your session. Usage: quit"
-#		self.terminal.write("Thanks for playing!")
-#		self.terminal.nextLine()
-#		self.terminal.loseConnection()
-#	def do_clear(self):
-#		"Clears the screen. Usage: clear"
-#		self.terminal.reset()
 
 class SSHDemoAvatar(avatar.ConchUser):
 	implements(conchinterfaces.ISession)
