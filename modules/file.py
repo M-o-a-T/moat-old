7fc0e74c77c44fbb6ee85695cddac55b772eb3ee
Matthias Urlichs <smurf@smurf.noris.de>
2007-06-14 22:07:56 +0200

    Remove "file exists" check from modules/file -- provided by modules/path

--- modules/file.py
+++ modules/file.py
@@ -4,31 +4,8 @@
 """\
 This code reads a config file.
 
-Currently, it understands:
-
 	include NAME
-		- read that file too
-
-	load dir
-		- list module directories
-	load dir "NAME"
-		- add a module directory
-	load dir - "NAME"
-		- remove a module directory
-
-	load NAME
-		- load the module/package homevent.NAME
-
-	unload NAME
-		- ... and remove it again.
-
-#	config NAME:
-#		foo bar baz
-#		- pass these lines to this module's config() code
-#		- see there for further documentation
-
-Modules can register more words.
-#Of particular interest are the switchboard and timer modules.
+		- read that file
 
 """
 
@@ -37,15 +14,6 @@ from homevent.module import Module
 from homevent.check import Check,register_condition,unregister_condition
 from homevent.parser import read_config
 import os
-
-
-class FileExistsCheck(Check):
-	name=("exists","file")
-	doc="check if a handler exists"
-	def check(self,*args):
-		if len(args) != 1:
-			raise SyntaxError("Usage: if exists file 'name'")
-		return os.path.isfile(args[0])
 
 
 class Include(Statement):
@@ -72,10 +40,8 @@ class FileModule(Module):
 
 	def load(self):
 		main_words.register_statement(Include)
-		register_condition(FileExistsCheck)
 	
 	def unload(self):
 		main_words.unregister_statement(Include)
-		unregister_condition(FileExistsCheck)
 	
 init = FileModule
--- test/misc.py
+++ test/misc.py
@@ -20,7 +20,7 @@ block:
 	trigger bar3
 wait for 0.1
 block:
-	if exists file "misc2":
+	if file exists "misc2":
 		include "misc2"
 	else:
 		include "test/misc2"
@@ -31,6 +31,7 @@ h.main_words.register_statement(ShutdownHandler)
 load_module("trigger")
 load_module("wait")
 load_module("block")
+load_module("path")
 load_module("file")
 load_module("ifelse")
 
