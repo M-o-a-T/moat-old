40f10229706db343a95a53a4adbfb6b938fbd7f9
Matthias Urlichs <smurf@smurf.noris.de>
2007-11-19 04:02:25 +0100

    Allow (singular) incoming TCP connections

--- modules/net.py
+++ modules/net.py
@@ -13,11 +13,9 @@ from homevent.run import process_failure
 from homevent.context import Context
 from homevent.event import Event
 from homevent.run import process_event,process_failure
-from homevent.reconnect import ReconnectingClientFactory
-from homevent.twist import deferToLater
 
 from twisted.python import failure
-from twisted.internet import protocol,defer,reactor
+from twisted.internet import protocol,reactor,error
 from twisted.protocols.basic import LineReceiver,_PauseableMixin
 
 import os
@@ -79,13 +77,14 @@ class NETreceiver(object,LineReceiver, _PauseableMixin):
 	def loseConnection(self):
 		if self.transport:
 			self.transport.loseConnection()
+		if self.factory:
+			self.factory.lostConnection(self)
 	
 	def write(self,val):
 		self.transport.write(val+self.delimiter)
 
 
-class NETfactory(object,ReconnectingClientFactory):
-
+class NETcommon_factory(object):
 	protocol = NETreceiver
 
 	def __init__(self, host="localhost", port=4304, name=None, *a,**k):
@@ -100,36 +99,65 @@ class NETfactory(object,ReconnectingClientFactory):
 		self.trace_retry = NETLOG
 
 	def haveConnection(self,conn):
+		self.drop()
 		self.conn = conn
 
 		if not self.up_event:
 			self.up_event = True
 			process_event(Event(Context(),"net","connect",self.name)).addErrback(process_failure)
 
+	def lostConnection(self,conn):
+		if self.conn == conn:
+			self.conn = None
+			self._down_event()
+
+	def _down_event(self):
+		if self.up_event:
+			self.up_event = False
+			process_event(Event(Context(),"net","disconnect",self.name)).addErrback(process_failure)
+
 	def drop(self):
 		"""Kill my connection"""
 		if self.conn:
 			self.conn.loseConnection()
-		self.conn = None
 		
-	def clientConnectionFailed(self, connector, reason):
-		self.conn = None
-		log(WARN,reason)
-		super(NETfactory,self).clientConnectionFailed(connector, reason)
-		if self.up_event:
-			self.up_event = False
-			process_event(Event(Context(),"net","disconnect", self.name), return_errors=True).addErrback(process_failure)
-
-	def clientConnectionLost(self, connector, reason):
-		self.conn = None
-		log(INFO,reason)
-		connector.connect()
-
 	def write(self,val):
 		if self.conn:
 			self.conn.write(val)
 		else:
 			raise DisconnectedError(self.name)
+
+	def end(self):
+		c = self.conn
+		self.conn = None
+		if c:
+			c.loseConnection()
+			self._down_event()
+
+class NETserver_factory(NETcommon_factory,protocol.ServerFactory):
+	def end(self):
+		try: self._port.stopListening()
+		except AttribteError: pass # might be called twice
+		del self._port
+		super(NETserver_factory,self).end()
+
+class NETclient_factory(NETcommon_factory,protocol.ClientFactory):
+	def end(self):
+		try: self.connector.stopConnecting()
+		except error.NotConnectingError: pass
+		del self.connector
+		super(NETclient_factory,self).end()
+
+	def clientConnectionFailed(self, connector, reason):
+		log(WARN,reason)
+		self.conn = None
+		self._down_event()
+
+	def clientConnectionLost(self, connector, reason):
+		log(INFO,reason)
+		self.conn = None
+		self._down_event()
+
 
 
 net_conns = {}
@@ -139,21 +167,32 @@ def connect(host="localhost", port=None, name=None):
 	assert name is not None, "Need to provide a name"
 	assert (host,port) not in net_conns, "already known host/port tuple"
 	assert name not in net_conns, "already known name"
-	f = NETfactory(host=host, port=port, name=name)
+	f = NETclient_factory(host=host, port=port, name=name)
 	net_conns[(host,port)] = f
 	net_conns[name] = f
-	reactor.connectTCP(host, port, f)
+	f.connector = reactor.connectTCP(host, port, f)
+	return f
+
+def listen(host="localhost", port=None, name=None):
+	assert port is not None, "Need to provide a port number"
+	assert name is not None, "Need to provide a name"
+	assert (host,port) not in net_conns, "already known host/port tuple"
+	assert name not in net_conns, "already known name"
+	f = NETserver_factory(host=host, port=port, name=name)
+	net_conns[(host,port)] = f
+	net_conns[name] = f
+	f._port = reactor.listenTCP(port, f, interface=host)
 	return f
 
 def disconnect(f):
 	assert f==net_conns.pop((f.host,f.port))
 	assert f==net_conns.pop(f.name)
-	f.drop()
+	f.end()
 
 
 class NETconnect(Statement):
 	name = ("connect","net")
-	doc = "connect to an NET server"
+	doc = "connect to a TCP port"
 	long_doc="""\
 connect net NAME [host] port
 - connect (asynchronously) to the TCP server at the remote port;
@@ -173,6 +212,32 @@ connect net NAME [host] port
 		port = event[-1]
 
 		connect(host,port,name)
+
+
+class NETlisten(Statement):
+	name = ("listen","net")
+	doc = "listen to a TCP socket"
+	long_doc="""\
+listen net NAME [address] port
+- listen (asynchronously) on the given port for a TCP connection.
+	name that connection NAME. Default for address is loopback.
+	The system will emit a connection-ready event when a client
+	connects. If another client connects, the old one will be
+	disconnected.
+"""
+
+	def run(self,ctx,**k):
+		event = self.params(ctx)
+		if len(event) < 2 or len(event) > 3:
+			raise SyntaxError("Usage: connect net ‹name› ‹host›? ‹port›")
+		name = event[0]
+		if len(event) == 2:
+			host = "localhost"
+		else:
+			host = event[1]
+		port = event[-1]
+
+		listen(host,port,name)
 
 
 class NETsend(Statement):
@@ -236,7 +301,7 @@ list net ‹name›?
 
 class NETconnected(Check):
 	name=("connected","net")
-	doc="Test if a net server connection is up"
+	doc="Test if a TCP connection is up"
 	def check(self,*args):
 		if len(args) == 1:
 			conn = net_conns.get(args[0],None)
@@ -250,7 +315,7 @@ class NETconnected(Check):
 
 class NETexists(Check):
 	name=("exists","net")
-	doc="Test if a net server connection is configured"
+	doc="Test if a TCP connection is configured"
 	def check(self,*args):
 		if len(args) == 1:
 			return args[0] in net_conns
@@ -267,6 +332,7 @@ class NETmodule(Module):
 	info = "Basic line-based TCP access"
 
 	def load(self):
+		main_words.register_statement(NETlisten)
 		main_words.register_statement(NETconnect)
 		main_words.register_statement(NETdisconnect)
 		main_words.register_statement(NETsend)
@@ -275,6 +341,7 @@ class NETmodule(Module):
 		register_condition(NETconnected)
 	
 	def unload(self):
+		main_words.unregister_statement(NETlisten)
 		main_words.unregister_statement(NETconnect)
 		main_words.unregister_statement(NETdisconnect)
 		main_words.unregister_statement(NETsend)
--- test/net.py
+++ test/net.py
@@ -7,14 +7,24 @@ from homevent.module import load_module,Load,ModuleExists
 from test import run
 
 input = """\
+on net connect foo:
+	send net foo "bar"
+	list net foo
+	disconnect net foo
+on net connect baz:
+	send net baz "quux"
+	list net baz
+	disconnect net baz
+on net disconnect foo:
+	log TRACE dis foo
+on net disconnect baz:
+	log TRACE dis baz
 wait for 0.2
+listen net baz localhost 50345
 connect net foo localhost 50333
-wait for 1
+wait for 0.8
+log TRACE ending
 list net
-list net foo
-send net foo "bar"
-wait for 1.1
-disconnect net foo
 shutdown
 """
 
@@ -23,6 +33,8 @@ h.main_words.register_statement(Load)
 h.register_condition(ModuleExists)
 
 load_module("wait")
+load_module("logging")
+load_module("on_event")
 load_module("net")
 
 run("net",input)
