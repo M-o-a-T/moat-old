04d36ac3a243792aad0b3d0db52821dbf4169a72
Matthias Urlichs <smurf@smurf.noris.de>
2007-12-23 23:19:58 +0100

    Added copyright notes

--- homevent/base.py
+++ homevent/base.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
+
+##
+##  Copyright (C) 2007  Matthias Urlichs <matthias@urlichs.de>
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License (included; see the file LICENSE)
+##  for more details.
+##
+
 """\
 	This module holds a few random constants and stuff.
 	"""
+
 SYS_PRIO = -10
 MIN_PRIO = 0
 MAX_PRIO = 100
