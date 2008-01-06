17f1d737fed80bb933c82acd2709c8ae7a0e422c
Matthias Urlichs <smurf@smurf.noris.de>
2008-01-06 17:27:17 +0100

    test the FS20-EM receiver (we don't have a sender yet!)

--- test/audio.sh
+++ test/audio.sh
@@ -23,5 +23,5 @@ else echo "No audio subdir"; exit 1; fi
 
 make
 
-./writer rate 32000 fs20 exec cat < ../test/expect/fs20rw | ./reader rate 32000 fs20 exec cat > ../test/real/fs20rw
+./writer rate 32000 em fs20 exec cat < ../test/expect/fs20rw | ./reader rate 32000 em fs20 exec cat > ../test/real/fs20rw
 
