ec1ab09a26d64e1e865e73476ed900ae1f9a7b2e
Matthias Urlichs <matthias@urlichs.de>
2012-04-29 10:39:13 +0200

    Add reasonable options (and output) to listschedule command

--- irrigation/rainman/management/commands/listschedule.py
+++ irrigation/rainman/management/commands/listschedule.py
@@ -20,49 +20,109 @@
 		"""
 
 from django.core.management.base import BaseCommand, CommandError
-from rainman.models import Site,Valve,Schedule
+from rainman.models import Site,Valve,Schedule,Controller
 from datetime import datetime,time,timedelta
-from django.db.models import F
+from django.db.models import F,Q
 from django.utils.timezone import utc
+from optparse import make_option
+
 
 class Command(BaseCommand):
 	args = '<site> <interval> <valve>…'
 	help = 'Report the schedule for the given valves, or all of them'
 
-	def handle(self, *args, **options):
-		if len(args):
-			try:
-				site = Site(name=args[0])
-			except Site.DoesNotExist:
-				raise CommandError('Site "%s" does not exist' % args[0])
-			args = args[1:]
-			self.one_site(False,site,args)
-		else:
-			for site in Site.objects.all():
-				self.one_site(True,site,())
+	option_list = BaseCommand.option_list + (
+			make_option('-s','--site',
+				action='store',
+				dest='site',
+				default=None,
+				help='Select the site to report a schedule for'),
+			make_option('-c','--controller',
+				action='store',
+				dest='controller',
+				default=None,
+				help='Select the controller to report a schedule for (may require --site)'),
+			make_option('-i','--interval',
+				action='store',
+				type=float,
+				dest='interval',
+				default=None,
+				help='report the next N days'),
+			make_option('-n','--current',
+				action='store_true',
+				dest='current',
+				default=False,
+				help='Also report currently-running activities'),
+			make_option('-a','--age',
+				action='store',
+				type=float,
+				dest='age',
+				default=None,
+				help='History: report N days back, up to the present'),
+			)
 
-	def one_site(self,name_site,site,args):
-		interval = 1
-		printed = False
-		#now=datetime.utcnow()
+	def handle(self, *args, **options):
+		q = Q()
 		now = datetime.utcnow().replace(tzinfo=utc)
-		for x in args:
-			try:
-				interval = float(x)
-				continue
-			except ValueError:
-				try:
-					valve = Valve(controller__site=site,name=x)
-				except Valve.DoesNotExist:
-					raise CommandError('Site "%s" does not have a valve "%s"' % (site.name,x))
-			start=now-timedelta(1)
-			printed = True
-			for sched in Schedule.objects.filter(valve=valve,start__gt=now-F('duration'),start__lt=now+timedelta(interval)):
-				if name_site:
-					print site.name,
-				print sched.start,sched.duration
-		if not printed:
-			for sched in Schedule.objects.filter(valve__controller__site=site,start__gt=now-F('duration'),start__lt=now+timedelta(interval)):
-				if name_site:
-					print site.name,
-				print sched.valve.name,sched.start,sched.duration
+		if options['site']:
+			q &= Q(controller__site__name=options['site'])
+		if options['controller']:
+			q &= Q(controller__name=options['controller'])
+		if options['age']:
+			if options['interval']:
+				raise CommandError("The '--interval' and '--age' options are mutually excusive")
+			q &= Q(start__gte=now-timedelta(float(options['age'])))
+			q &= Q(start__lt=now)
+		else:
+			if options['interval']:
+				iv = float(options['interval'])
+			else:
+				iv = 1
+			if options['current']:
+				q &= Q(start__gt=now-timedelta(1)) # this uses the index
+				q &= Q(start__gt=now-F(duration))
+			else:
+				q &= Q(start__gte=now)
+			q &= Q(start__lt=now+timedelta(iv))
+
+		if len(args):
+			for a in args:
+				qq = Q(valve__name=a)
+				self.one_site(q & qq, options)
+		else:
+			self.one_site(q,options)
+
+	def one_site(self,q,options):
+		"""Print results as a nice minimal table"""
+		r = []
+		if 'site' not in options:
+			r.append("Site")
+		if 'controller' not in options:
+			r.append("Controller")
+		r.extend(("Valve","Start","Dauer"))
+		res = [r]
+		lengths = [0] * len(r)
+		for sched in Schedule.objects.filter(q):
+			r = []
+			if 'site' not in options:
+				r.append(sched.controller.site.name)
+			if 'controller' not in options:
+				r.append(sched.controller.name)
+			r.extend((sched.valve.name, str(sched.start), str(sched.duration)))
+		for a in res:
+			i=0
+			for b in a:
+				lb = len(b)
+				if lengths[i] < lb:
+					lengths[i] = lb
+				i += 1
+		for a in res:
+			r = []
+			i=0
+			for b in a:
+				r.append(b + " "*(lengths[i]-len(b)))
+				i += 1
+			print " ".join(r)
+			
+
+
