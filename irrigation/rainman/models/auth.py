15c8dd9c3b05bbc6ba870e6546eb1197616ff6c0
Matthias Urlichs <matthias@urlichs.de>
2012-05-24 07:58:30 +0200

    Repaur auth.UserForGroup as per Django docs

--- irrigation/rainman/models/auth.py
+++ irrigation/rainman/models/auth.py
@@ -18,6 +18,7 @@ from __future__ import division,absolute_import
 from rainman.models import Model
 from rainman.models.group import Group
 from django.db import models as m
+from django.db.models.signals import post_save
 
 from django.contrib.auth.models import User as DjangoUser
 class UserForGroup(Model):
@@ -26,8 +27,8 @@ class UserForGroup(Model):
 		db_table="rainman_userforgroup"
 	def __unicode__(self):
 		return u"‹%s @%s %s›" % (self.__class__.__name__,self.user.username,self.group.name)
-	user = m.ForeignKey(DjangoUser)
-	group = m.ForeignKey(Group,related_name="users")
+	user = m.OneToOneField(DjangoUser)
+	group = m.ForeignKey(Group, blank=True,null=True, related_name="users",help_text="A group containing the valves this user may access")
 	LEVEL_VALUES = (
 		('0',"None"),
 		('1',"read"),
@@ -37,3 +38,11 @@ class UserForGroup(Model):
 	level = m.IntegerField(choices=LEVEL_VALUES,default=1,help_text=u"Access to …")
 
 
+# definition of UserProfile from above
+# ...
+
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		UserForGroup.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=DjangoUser)
