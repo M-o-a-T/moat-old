a87dc356f1ac81139837ad5225631c4f60a423ae
Matthias Urlichs <matthias@urlichs.de>
2014-11-26 14:43:56 +0100

    * Teach homevent-amqp to send messages

--- scripts/amqpmon.py
+++ scripts/amqpmon.py
@@ -39,7 +39,7 @@ import amqp
 import json
 from pprint import pprint
 
-modes = "log,list,cmd".split(",")
+modes = "log,msg".split(",")
 from optparse import OptionParser
 parser = OptionParser(conflict_handler="resolve")
 parser.add_option("-h","--help","-?", action="help",
@@ -53,27 +53,47 @@ parser.add_option("-p", "--pass", dest="pw", action="store",
 parser.add_option("-v", "--vhost", dest="vhost", action="store",
 	default="/test", help="Virtual host to connect at")
 parser.add_option("-x", "--exchange", dest="exchange", action="store",
-	default="homevent_event", help="Exchange to listen at")
+	default="homevent.event", help="Exchange to listen at")
+parser.add_option("-r", "--routing", dest="routing", action="store",
+	default="cmdline.generic", help="Routing key to send to")
+parser.add_option("-t", "--content-type", dest="content_type", action="store",
+	default="text/plain", help="The message's content type")
 
 (opts, args) = parser.parse_args()
 
-def on_msg(msg):
-	try:
-		msg.body = json.loads(msg.body)
-	except Exception:
-		pass
-	pprint(msg.__dict__)
-
 conn = amqp.connection.Connection(host=opts.host, userid=opts.user, password=opts.pw, login_method='AMQPLAIN', login_response=None, virtual_host=opts.vhost)
-chan = conn.channel()
-res = chan.exchange_declare(exchange=opts.exchange, type='topic', auto_delete=False, passive=False)
-res = chan.queue_declare(exclusive=True)
-chan.queue_bind(exchange=opts.exchange, queue=res.queue, routing_key="#")
-chan.basic_consume(callback=on_msg, queue=res.queue, no_ack=True)
 
-def main(opts,args):
+def main(conn,opts,args):
+	if not args:
+		raise SyntaxError("set a mode (%s)" % (", ".join(modes),))
+	mode = args[0]
+	args = args[1:]
+	if mode == "log":
+		do_log(conn)
+	elif mode == "msg":
+		do_msg(conn,args)
+
+def do_msg(conn,args):
+	arg = " ".join(args)
+	chan = conn.channel()
+	msg = amqp.Message(body=arg, content_type=opts.content_type)
+	chan.basic_publish(msg=msg, exchange=opts.exchange, routing_key=opts.routing)
+
+def do_log(conn):
+	def on_msg(msg):
+		try:
+			msg.body = json.loads(msg.body)
+		except Exception:
+			pass
+		pprint(msg.__dict__)
+
+	chan = conn.channel()
+	res = chan.exchange_declare(exchange=opts.exchange, type='topic', auto_delete=False, passive=False)
+	res = chan.queue_declare(exclusive=True)
+	chan.queue_bind(exchange=opts.exchange, queue=res.queue, routing_key="#")
+	chan.basic_consume(callback=on_msg, queue=res.queue, no_ack=True)
+
 	sig = Event()
-
 	def run():
 		while not sig.isSet():
 			conn.drain_events()
@@ -90,7 +110,7 @@ def main(opts,args):
 
 if __name__ == "__main__":
 	try:
-		main(opts,args)
+		main(conn,opts,args)
 	except Exception:
 		raise
 	except BaseException:
