9bdf76fe3373089becbe26a33f2bd283d320d2b3
Matthias Urlichs <matthias@urlichs.de>
2015-04-16 22:54:16 +0200

    manage: Init gevent
    except when using Django, Ouch.

---
+++ irrigation/manage2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from homevent import patch;patch()
+from homevent import twist
+import os
+import sys
+
+
+#from django.conf import settings
+#import settings as s
+#settings.configure(s)
+
+### this import must not happen before settings are completely loaded
+from hamlish_jinja import Hamlish
+Hamlish._self_closing_jinja_tags.add('csrf_token')
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
--- irrigation/run.sh
+++ irrigation/run.sh
@@ -4,5 +4,5 @@ export DJANGO_SETTINGS_MODULE="settings"
 export PYTHONPATH=$(pwd)/..:$(pwd)
 (
 python manage.py runserver 0.0.0.0:58000 &
-python manage.py runschedule Schleiermacher+Hardenberg &
+python manage2.py runschedule Schleiermacher+Hardenberg &
 ) > /tmp/rainman.log 2>&1
