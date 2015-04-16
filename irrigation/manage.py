b3f597abfbb5c403ef862d8e1256b879084bae29
Matthias Urlichs <matthias@urlichs.de>
2015-04-16 22:53:46 +0200

    drop superfluous imports

--- irrigation/manage.py
+++ irrigation/manage.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
+#from homevent import patch;patch()
 from homevent import twist
 import os
 import sys
-
-from django.conf import settings
-import settings as s
-settings.configure(s)
 
 ### this import must not happen before settings are completely loaded
 from hamlish_jinja import Hamlish
