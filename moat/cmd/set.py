36a678db90629c42d57f08838691d4bda21ee87d
Matthias Urlichs <matthias@urlichs.de>
2015-11-15 12:37:30 +0100

    command update

---
+++ moat/cmd/set.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, division, unicode_literals
+##
+##  This file is part of MoaT, the Master of all Things.
+##
+##  MoaT is Copyright © 2007-2015 by Matthias Urlichs <matthias@urlichs.de>,
+##  it is licensed under the GPLv3. See the file `README.rst` for details,
+##  including optimistic statements by the author.
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License (included; see the file LICENSE)
+##  for more details.
+##
+##  This header is auto-generated and may self-destruct at any time,
+##  courtesy of "make update". The original is in ‘scripts/_boilerplate.py’.
+##  Thus, do not remove the next line, or insert any blank lines above.
+##BP
+
+"""List of known Tasks"""
+
+import os
+import sys
+from moat.script import Command, CommandError
+from etctree.util import from_etcd
+import aioetcd as etcd
+
+__all__ = ['SetCommand']
+
+class EtcdCommand(Command):
+	name = "etcd"
+	summary = "Update etcd data"""
+	description = """\
+Add/Update/Delete etcd contents.
+
+Usage (unless deleting): path.to.entry=value
+If you want to modify an entry, the '-m' or '-p' option is mandatory.
+
+"""
+
+	def addOptions(self):
+		self.parser.add_option('-a','--append',
+            action="store_true", dest="append",
+            help="append a unique entry")
+		self.parser.add_option('-d','--delete',
+            action="store_true", dest="delete",
+            help="delete entries instead of updating them")
+		self.parser.add_option('-m','--modified',
+            action="store", dest="modified",
+            help="Modification stamp of current entry.")
+		self.parser.add_option('-p','--prev',
+            action="store", dest="previous",
+            help="Previous value of current entry.")
+
+	def handleOptions(self,opts):
+		if (opts.delete or opts.modified or opts.previous) and opts.append:
+			raise CommandError("Conflicting arguments")
+		self.delete = opts.delete
+		self.modified = opts.modified
+		self.previous = opts.previous
+		self.append = opts.append
+
+	def do(self,args):
+		from yaml import safe_dump
+		if not args:
+			raise CommandError("Not specifying values to %s makes no sense." % ("delete" if self.delete else "add/update"))
+		if self.delete:
+			for a in args:
+				pa = a.replace('.','/')
+				kw = {}
+				if self.modified:
+					kw['prevIndex'] = self.modified
+				if self.previous:
+					kw['prevValue'] = self.previous
+				try:
+					self.root.sync(self.root.etcd.delete('/'+pa, recursive=True,**kw))
+				except etcd.EtcdCompareFailed:
+					print("Bad modstamp: /"+a, file=sys.stderr)
+				except etcd.EtcdKeyNotFound:
+					pass
+		else:
+			for a in args:
+				a,v = a.split('=',1)
+				pa = a.replace('.','/')
+				try:
+					kw={}
+					if self.modified:
+						kw['index']=self.modified
+					if self.previous:
+						kw['prev']=self.previous
+					if v:
+						kw['value']=v
+					if self.append:
+						kw['append']=True
+					r = self.root.sync(self.root.etcd.set('/'+pa, prev=None, **kw))
+					if self.append:
+						print(r.key.rsplit('/',1)[1])
+				except etcd.EtcdCompareFailed:
+					print("Bad modstamp: "+a, file=sys.stderr)
+				except etcd.EtcdAlreadyExist:
+					print("Entry exists: "+a, file=sys.stderr)
+
+class SetCommand(Command):
+	name = "set"
+	summary = "Update data"""
+	description = """\
+Set some data.
+"""
+
+	subCommandClasses = [EtcdCommand]
+
---
+++ moat/cmd/test.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, division, unicode_literals
+##
+##  This file is part of MoaT, the Master of all Things.
+##
+##  MoaT is Copyright © 2007-2015 by Matthias Urlichs <matthias@urlichs.de>,
+##  it is licensed under the GPLv3. See the file `README.rst` for details,
+##  including optimistic statements by the author.
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License (included; see the file LICENSE)
+##  for more details.
+##
+##  This header is auto-generated and may self-destruct at any time,
+##  courtesy of "make update". The original is in ‘scripts/_boilerplate.py’.
+##  Thus, do not remove the next line, or insert any blank lines above.
+##BP
+
+"""List of known Tasks"""
+
+import os
+import sys
+from moat.script import Command, CommandError
+from etctree.util import from_etcd
+import aioetcd as etcd
+import asyncio
+import logging
+
+__all__ = ['TestCommand']
+
+class ConfigCommand(Command):
+	name = "config"
+	summary = "Verify configuration data"""
+	description = """\
+Check basic config layout.
+
+"""
+
+	def do(self,args):
+		c = self.root.cfg
+		try:
+			c = c['config']
+		except KeyError:
+			raise CommandError("config: missing 'config' entry")
+
+class EtcdCommand(Command):
+	name = "etcd"
+	summary = "Verify etcd data"""
+	description = """\
+Check etcd access, and basic data layout.
+
+"""
+
+	def do(self,args):
+		c = self.root.cfg['config']
+		try:
+			c = c['etcd']
+		except KeyError:
+			raise CommandError("config: missing 'etcd' section")
+		for k in ('host','port','root'):
+			if k not in c:
+				raise CommandError("config.etcd: missing '%s' entry" % k)
+		self.root.sync(self._do())
+	
+	@asyncio.coroutine
+	def _do(self):
+		etcd = yield from self.root._get_etcd()
+		log = logging.getLogger("etcd")
+		stats = set(("ok","warn","error","fail"))
+		s = yield from etcd.tree("/status")
+		if "run" not in s:
+			log.error("missing 'run' entry")
+			if self.parent.fix:
+				yield from s.set("run",dict())
+		if "errors" not in s:
+			log.error("missing 'errors' entry")
+			if self.parent.fix:
+				yield from s.set("errors",dict((s,0) for s in stats))
+		else:
+			err = s['errors']
+			for stat in stats:
+				if stat not in err:
+					log.error("missing 'errors.%s' entry" % stat)
+					if self.parent.fix:
+						yield from err.set(stat,0)
+
+		with self.state("test.error","warn","Running tests")
+		try:
+		yield from s._wait()
+		errs = yield from etcd.read("/errors")
+		import pdb;pdb.set_trace()
+		pass
+
+
+
+class AmqpCommand(Command):
+	name = "amqp"
+	summary = "Verify amqp data"""
+	description = """\
+Check amqp access, and basic queue layout.
+
+Note that starting the AMQP connector automatically creates the requisite
+exchanges and queues.
+
+"""
+
+	def do(self,args):
+		pass
+
+class TestCommand(Command):
+	name = "test"
+	summary = "Run various tests"""
+	description = """\
+Set some data.
+"""
+
+	# process in order
+	subCommandClasses = [
+		ConfigCommand,
+		EtcdCommand,
+		AmqpCommand,
+	]
+	fix = False
+
+	def addOptions(self):
+		self.parser.add_option('-f','--fix',
+            action="store_true", dest="fix",
+            help="try to fix problems")
+
+	def handleOptions(self,opts):
+		self.fix = opts.fix
+	
+	def do(self,args):
+		if self.root.cfg['config'].get('testing',False):
+			print("NOTE: this is a test configuration.", file=sys.stderr)
+
+		for c in self.subCommandClasses:
+			if self.root.verbose > 1:
+				print("Checking:",c.name)
+			c = c(parent=self)
+			try:
+				c.do(args)
+			except Exception as exc:
+				if self.root.verbose > 1:
+					import traceback
+					traceback.print_exc(file=sys.stderr)
+					return
+				raise CommandError("%s failed: %s" % (c.name,repr(exc)))
+		if self.root.verbose:
+			print("All tests done.")
+		
