7319091d43a2b1ee392335e8086e388b6bb8754e
Matthias Urlichs <matthias@urlichs.de>
2016-07-21 00:33:25 +0200

    Install the "new" moat command

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ additional I/O, logging, and easy integration into existing systems.
 	zip_safe = False, 
     packages = [str(x) for x in ('moat','moat.modules')],
 	package_dir={'moat': 'moat', 'moat.modules':'modules'},
-    scripts = [str(x) for x in ('scripts/daemon','scripts/cmd','scripts/amqpmon')]
+    scripts = [str(x) for x in ('scripts/daemon','scripts/moat','scripts/amqpmon')]
     #cmdclass={'install_data' : my_install_data},
     )
